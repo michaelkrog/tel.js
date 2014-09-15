d6d9e8f472e6cd1bc11f7c6806dd4e699d34947b
Michael Krog <mic@apaq.dk>
2014-09-15 10:45:25 +0200

    Added support for  and  on the returned number

--- src/tel.js
+++ src/tel.js
@@ -15,48 +15,23 @@ if (window.goog === undefined) {
 }
 /* Fallback if google protocols is not defined - END*/
 
-var teljs = angular.module('teljs',[]);
-teljs.trimNumber = function(value) {
-    if(value) {
-        return value.replace(/[\+\s\-\(\)]/g, '');
-    } else {
-        return value;
-    }
-};
-
-teljs.filter('telephone', function() {
+function telephoneFilter() {
     
-    var countryCodes = i18n.phonenumbers.metadata.countryCodeToRegionCodeMap;//teljs.countryAreaCodes;
-            
-    // [<areacode>, <nationalPrefix>, [[<formatPattern>, <leadingDigits<, <format>, <intlFormat>],...]]
-    /*
-    * [<0:PhoneMetadata>,
-    *     [<unknown>,<unknown>,<nationalNumberPattern>,<possibleNumberPattern>], (1:generalDesc)
-    *     [<unknown>,<unknown>,<nationalNumberPattern>,<possibleNumberPattern>], (2:fixedLine)
-    *     [<unknown>,<unknown>,<nationalNumberPattern>,<possibleNumberPattern>], (3:mobile)
-    *     [<unknown>,<unknown>,<nationalNumberPattern>,<possibleNumberPattern>], (4:tollfree)
-    *     [<unknown>,<unknown>,<nationalNumberPattern>,<possibleNumberPattern>], (5:premiumrate)
-    *     [,,"NA","NA"], (6:shared_cost)
-    *     [,,"NA","NA"], (7:personal_number)
-    *     [,,"NA","NA"], (8:voip)
-    *     <9:id>,<10:countryCode>,<11:internationalPrefix>,<12:national_prefix>,<13:preferred_extn_prefix>,
-    *     <14:unknown>,<15:national_prefix_for_parsing>,<16:national_prefix_transform_rule>,
-    *     <17:preferred_international_prefix>,<18:same_mobile_and_fixed_line_pattern>,
-    *     [ (19:numberformat)
-    *         [<unknown>,<pattern>,<format>,[<leadingdigits>],<nationalPrefixFormattingRule>,"",0]  (numberformat)
-    *     ],
-    *     [ (20:internation numberformat)
-    *         [<unknown>,<pattern>,<format>,[<leadingdigits>]]  (numberformat)
-    *     ],
-    *     [<unknown>,<unknown>,"NA","NA"], (21:pager)
-    *     <22:main_country_for_code>,<23:leading_digits>,
-    *     [<unknown>,<unknown>,"NA","NA"], (24:no_international_dialling)
-    *     [<unknown>,<unknown>,"NA","NA"], (25:uan)
-    *     <26:leading_zero_possible>,<27:emergency>,
-    *     [<unknown>,<unknown>,"NA","NA"] (28:voicemail)
-    * ]
-    */
-    var countryMetaData = i18n.phonenumbers.metadata.countryToMetadata; //teljs.meta;
+    var countryCodes = i18n.phonenumbers.metadata.countryCodeToRegionCodeMap;
+    var countryMetaData = i18n.phonenumbers.metadata.countryToMetadata;
+    var validRangeMap = {
+        2: "fixedLine",
+        3: "mobile",
+        4: "tollFree",
+        5: "premiumRate",
+        6: "sharedCost",
+        7: "personalNumber",
+        8: "voip",
+        21: "pager",
+        24: "noInternationalDialing",
+        25: "uan",
+        28: "voiceMail"
+    };
     
     var regionsFromNumber = function(e164) {
         var regions, key, value;
@@ -84,7 +59,7 @@ teljs.filter('telephone', function() {
             internationalFormats = metaData[20],
             number, international = (mode === 'e164'),
             numberFormats = international && internationalFormats ? internationalFormats : nationalFormats,
-            entry, i, matchNumber, matchLeadingDigits, validRange = false;
+            entry, i, matchNumber, matchLeadingDigits, validRange = false, range;
 
         if(numberFormats) {
             
@@ -92,12 +67,15 @@ teljs.filter('telephone', function() {
                 nationalNumber = nationalNumber.substr(nationalPrefix.length);
             }
             
-            angular.forEach([1,2,3,4,5,6,7,8,21,24,25,28], function(validRangeFormatIndex) {
-                entry = metaData[validRangeFormatIndex];
+            for (var key in validRangeMap) {
+                entry = metaData[key];
                 if(new RegExp('^('+entry[2]+')$').exec(nationalNumber)) {
                     validRange = true;
+                    range = validRangeMap[key];
+                    break;
                 }
-            });
+            }
+
             if(validRange) {
                 for(i = 0;i<numberFormats.length;i++) {
                     entry = numberFormats[i];
@@ -121,14 +99,16 @@ teljs.filter('telephone', function() {
                         if(international) {
                             number = nationalNumber;
                             number = number.replace(new RegExp(entry[1]), format);
-                            number = '+' + countryCode + ' ' + number;
+                            number = new String('+' + countryCode + ' ' + number);
                         } else {
                             number = nationalNumber;
                             if(nationalPrefix) {
                                 number = nationalPrefix + '' + number;
                             }
-                            number = number.replace(new RegExp(entry[1]), format);
+                            new String(number = number.replace(new RegExp(entry[1]), format));
                         }
+                        number.$countryCode = region;
+                        number.$range = range;
                         break;
                     }
                 }
@@ -136,58 +116,46 @@ teljs.filter('telephone', function() {
         }
         return number;
     };
-
     
-    return function(input, mode, defaultAreaCode) {
-        
-        var regions, trimmedNumber, defaultGeneratedNumber, number, nationalNumber, i, countryCode, region, nationalPrefix;
-        mode = mode ? mode : 'e164';
-        trimmedNumber = teljs.trimNumber(input);
-        if(defaultAreaCode && defaultAreaCode !== '') {
-            defaultGeneratedNumber = defaultAreaCode + '' + trimmedNumber;
-            regions = regionsFromNumber(defaultGeneratedNumber);
-
-            if(regions) {
-                for(i=0;i<regions.length;i++) {
-                    region = regions[i];
-                    countryCode = countryMetaData[region][10];
-                    nationalPrefix = countryMetaData[region][12];
-                    nationalNumber = defaultGeneratedNumber.substr(countryCode.toString().length);
-                    if(nationalPrefix && nationalNumber.substr(0, nationalPrefix.length) !== nationalPrefix) {
-                        nationalNumber = nationalPrefix + '' + nationalNumber;
-                    }
-                    number = formatNumberForRegion(region, nationalNumber, mode);
-                    if(number) break;
-                }
-            }
-            if(number) return number;
-        }   
-        
-        regions = regionsFromNumber(trimmedNumber);
+    var formatNumber = function(number, mode) {
+        var regions = regionsFromNumber(number), region, i, countryCode, nationalPrefix, nationalNumber, result;
 
         if(regions) {
             for(i=0;i<regions.length;i++) {
                 region = regions[i];
                 countryCode = countryMetaData[region][10];
                 nationalPrefix = countryMetaData[region][12];
-                nationalNumber = trimmedNumber.substr(countryCode.toString().length);
+                nationalNumber = number.substr(countryCode.toString().length);
                 if(nationalPrefix && nationalNumber.substr(0, nationalPrefix.length) !== nationalPrefix) {
                     nationalNumber = nationalPrefix + '' + nationalNumber;
                 }
-                number = formatNumberForRegion(region, nationalNumber, mode);
-                if(number) break;
+                result = formatNumberForRegion(region, nationalNumber, mode);
+                if(result) break;
             }
         }
+        return result;
+    };
 
+    
+    return function(input, mode, defaultAreaCode) {
         
+        var trimmedNumber, defaultGeneratedNumber, number;
+        mode = mode ? mode : 'e164';
+        trimmedNumber = teljs.trimNumber(input);
+        if(defaultAreaCode && defaultAreaCode !== '') {
+            defaultGeneratedNumber = defaultAreaCode + '' + trimmedNumber;
+            number = formatNumber(defaultGeneratedNumber, mode);
+            if(number) return number;
+        }   
         
+        number = formatNumber(trimmedNumber, mode);
         return number;
 
 
     };
-});
+}
 
-teljs.directive('input', function ($filter) {
+function telephoneDirective($filter) {
     return {
         restrict: 'E', // only activate on element attribute
         require: '?ngModel', // get a hold of NgModelController
@@ -252,4 +220,19 @@ teljs.directive('input', function ($filter) {
 
         }
     };
-});
+}
+telephoneDirective.$inject = ['$filter'];
+
+
+/********** BOOTSTRAP ************/
+var teljs = angular.module('teljs',[]);
+teljs.trimNumber = function(value) {
+    if(value) {
+        return value.replace(/[\+\s\-\(\)]/g, '');
+    } else {
+        return value;
+    }
+};
+
+teljs.filter('telephone', telephoneFilter);
+teljs.directive('input', telephoneDirective);
